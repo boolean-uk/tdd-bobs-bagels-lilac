33be3d0363d0d48360173dc3e94476d688c552c8
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-28 15:49:49 +0100

    Read and commented on source code and test files

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,21 +1,23 @@
 const Basket = require("../src/basket.js");
 
 describe("Basket", () => {
-  let basket
+  let basket;
 
   beforeEach(() => {
     basket = new Basket();
   });
 
   it("create and add a item to the basket", () => {
-    const expected = [{
-      sku: "BGLO",
-      price: 0.49,
-      name: "Bagel",
-      variant: "Onion"
-  }]
-    basket.addItemToBasket('BGLO')  
-    const result = basket.showBasket()
+    const expected = [
+      {
+        sku: "BGLO",
+        price: 0.49,
+        name: "Bagel",
+        variant: "Onion",
+      },
+    ];
+    basket.addItemToBasket("BGLO");
+    const result = basket.showBasket();
     expect(result).toEqual(expected);
   });
 
@@ -25,18 +27,18 @@ describe("Basket", () => {
         sku: "BGLO",
         price: 0.49,
         name: "Bagel",
-        variant: "Onion"
+        variant: "Onion",
       },
       {
         sku: "BGLP",
         price: 0.39,
         name: "Bagel",
-        variant: "Plain"
-      }
-    ]
-    basket.addItemToBasket('BGLO')
-    basket.addItemToBasket('BGLP')
-    const result = basket.showBasket()
+        variant: "Plain",
+      },
+    ];
+    basket.addItemToBasket("BGLO");
+    basket.addItemToBasket("BGLP");
+    const result = basket.showBasket();
     expect(result).toEqual(expected);
   });
 
@@ -46,13 +48,13 @@ describe("Basket", () => {
         sku: "BGLP",
         price: 0.39,
         name: "Bagel",
-        variant: "Plain"
-      }
-    ]
-    basket.addItemToBasket('BGLO')
-    basket.addItemToBasket('BGLP')
-    basket.removeItemFromBasket('BGLO')
-    const result = basket.showBasket()
+        variant: "Plain",
+      },
+    ];
+    basket.addItemToBasket("BGLO");
+    basket.addItemToBasket("BGLP");
+    basket.removeItemFromBasket("BGLO");
+    const result = basket.showBasket();
     expect(result).toEqual(expected);
   });
 
@@ -62,85 +64,87 @@ describe("Basket", () => {
         sku: "BGLP",
         price: 0.39,
         name: "Bagel",
-        variant: "Plain"
+        variant: "Plain",
       },
       {
         sku: "BGLE",
         price: 0.49,
         name: "Bagel",
-        variant: "Everything"
-      }
-    ]
-    basket.addItemToBasket('BGLO')
-    basket.addItemToBasket('BGLP')
-    basket.addItemToBasket('BGLE')
-    basket.addItemToBasket('BGLS')
-    basket.removeItemFromBasket('BGLO')
-    basket.removeItemFromBasket('BGLS')
-    const result = basket.showBasket()
+        variant: "Everything",
+      },
+    ];
+    basket.addItemToBasket("BGLO");
+    basket.addItemToBasket("BGLP");
+    basket.addItemToBasket("BGLE");
+    basket.addItemToBasket("BGLS");
+    basket.removeItemFromBasket("BGLO");
+    basket.removeItemFromBasket("BGLS");
+    const result = basket.showBasket();
     expect(result).toEqual(expected);
   });
 
   it("a user cannot add more than 5 items to their basket", () => {
-    const expected = 'You cannot add more than 5 items to your basket!'
-    basket.addItemToBasket('BGLO')
-    basket.addItemToBasket('BGLP')
-    basket.addItemToBasket('BGLE')
-    basket.addItemToBasket('BGLS')
-    basket.addItemToBasket('BGSS')
-    const result = basket.addItemToBasket('BGSE')
+    const expected = "You cannot add more than 5 items to your basket!";
+    basket.addItemToBasket("BGLO");
+    basket.addItemToBasket("BGLP");
+    basket.addItemToBasket("BGLE");
+    basket.addItemToBasket("BGLS");
+    basket.addItemToBasket("BGSS");
+    const result = basket.addItemToBasket("BGSE");
     expect(result).toEqual(expected);
   });
 
+  // Comment is too long.
   it("if the user tries to add more than 5 items, the basket will be full, the overfill will be discarded and an error message will be displayed", () => {
     const expected = [
       {
         sku: "BGLO",
         price: 0.49,
         name: "Bagel",
-        variant: "Onion"
+        variant: "Onion",
       },
       {
         sku: "BGLP",
         price: 0.39,
         name: "Bagel",
-        variant: "Plain"
+        variant: "Plain",
       },
       {
         sku: "BGLE",
         price: 0.49,
         name: "Bagel",
-        variant: "Everything"
+        variant: "Everything",
       },
       {
         sku: "BGLS",
         price: 0.49,
         name: "Bagel",
-        variant: "Sesame"
+        variant: "Sesame",
       },
       {
         sku: "BGSE",
         price: 2.99,
         name: "Bagel Sandwich",
         variant: "Everything",
-      }
-    ]
-    basket.addItemToBasket('BGLO')
-    basket.addItemToBasket('BGLP')
-    basket.addItemToBasket('BGLE')
-    basket.addItemToBasket('BGLS')
-    basket.addItemToBasket('BGSE')
-    basket.addItemToBasket('BGLO')
-    const result = basket.showBasket('BGSE')
+      },
+    ];
+    // I would use a for loop to add these 6 bagels because code here is repetitive.
+    basket.addItemToBasket("BGLO");
+    basket.addItemToBasket("BGLP");
+    basket.addItemToBasket("BGLE");
+    basket.addItemToBasket("BGLS");
+    basket.addItemToBasket("BGSE");
+    basket.addItemToBasket("BGLO");
+    const result = basket.showBasket("BGSE");
     expect(result).toEqual(expected);
   });
 
   it("a user cannot remove an item that does not exist from the basket", () => {
-    const expected = 'The item does not exist in your basket!'
-    basket.addItemToBasket('BGLO')
-    basket.addItemToBasket('BGLP')
-    basket.addItemToBasket('BGLE')
-    const result = basket.removeItemFromBasket('BGLS')
+    const expected = "The item does not exist in your basket!";
+    basket.addItemToBasket("BGLO");
+    basket.addItemToBasket("BGLP");
+    basket.addItemToBasket("BGLE");
+    const result = basket.removeItemFromBasket("BGLS");
     expect(result).toEqual(expected);
   });
 
@@ -150,25 +154,25 @@ describe("Basket", () => {
         sku: "BGLO",
         price: 0.49,
         name: "Bagel",
-        variant: "Onion"
+        variant: "Onion",
       },
       {
         sku: "BGLP",
         price: 0.39,
         name: "Bagel",
-        variant: "Plain"
+        variant: "Plain",
       },
       {
         sku: "BGLE",
         price: 0.49,
         name: "Bagel",
-        variant: "Everything"
+        variant: "Everything",
       },
       {
         sku: "BGLS",
         price: 0.49,
         name: "Bagel",
-        variant: "Sesame"
+        variant: "Sesame",
       },
       {
         sku: "BGSE",
@@ -187,32 +191,32 @@ describe("Basket", () => {
         price: 2.99,
         name: "Bagel Sandwich",
         variant: "Nutella",
-      }
-    ]
-    basket.basketSize = 10
-    basket.addItemToBasket('BGLO')
-    basket.addItemToBasket('BGLP')
-    basket.addItemToBasket('BGLE')
-    basket.addItemToBasket('BGLS')
-    basket.addItemToBasket('BGSE')
-    basket.addItemToBasket('BGSS')
-    basket.addItemToBasket('BGSN')
-    const result = basket.showBasket()
+      },
+    ];
+    basket.basketSize = 10;
+    basket.addItemToBasket("BGLO");
+    basket.addItemToBasket("BGLP");
+    basket.addItemToBasket("BGLE");
+    basket.addItemToBasket("BGLS");
+    basket.addItemToBasket("BGSE");
+    basket.addItemToBasket("BGSS");
+    basket.addItemToBasket("BGSN");
+    const result = basket.showBasket();
     expect(result).toEqual(expected);
   });
 
   it("a user can check item price before adding it to the basket", () => {
-    const expected = 'The price of the item is £2.99'
-    const result = basket.getItemPrice('BGSN')
+    const expected = "The price of the item is £2.99";
+    const result = basket.getItemPrice("BGSN");
     expect(result).toEqual(expected);
   });
 
   it("a user knows the total price of the items in their basket", () => {
-    const expected = 'The total price of the items in your basket is £1.37'
-    basket.addItemToBasket('BGLO')
-    basket.addItemToBasket('BGLP')
-    basket.addItemToBasket('BGLS')
-    const result = basket.getBasketTotal()
+    const expected = "The total price of the items in your basket is £1.37";
+    basket.addItemToBasket("BGLO");
+    basket.addItemToBasket("BGLP");
+    basket.addItemToBasket("BGLS");
+    const result = basket.getBasketTotal();
     expect(result).toEqual(expected);
   });
 });
--- src/basket.js
+++ src/basket.js
@@ -1,48 +1,55 @@
 const inventory = require("./inventory.js");
 class Basket {
-    constructor() {
-        this.basket = [];
-        this.basketSize = 5;
-    };
-    
-    addItemToBasket(sku) {
-        for (let i = 0; i < inventory.length; i++) {
-            if (inventory[i].sku === sku && this.basket.length < this.basketSize) {
-                this.basket.push(inventory[i]);
-            }
-        }
-        return 'You cannot add more than 5 items to your basket!'
-    };
+  constructor() {
+    this.basket = [];
+    this.basketSize = 5;
+  }
+  // Only one class used. Maybe we could use more classes.
+  // this.price could have been used as a constructor.
 
-    removeItemFromBasket(sku) {
-        for (let i = 0; i < this.basket.length; i++) {
-            if (this.basket[i].sku === sku) {
-                this.basket.splice(i, 1)
-            }
-        }
-        return 'The item does not exist in your basket!'
+  addItemToBasket(sku) {
+    for (let i = 0; i < inventory.length; i++) {
+      if (inventory[i].sku === sku && this.basket.length < this.basketSize) {
+        this.basket.push(inventory[i]);
+      }
     }
+    return "You cannot add more than 5 items to your basket!";
+  }
 
-    getItemPrice(product) {
-        for (let i = 0; i < inventory.length; i++) {
-            if (inventory[i].sku === product) {
-                return `The price of the item is £${inventory[i].price}`
-            }
-        }
-        return 'The product is not available'
+  removeItemFromBasket(sku) {
+    for (let i = 0; i < this.basket.length; i++) {
+      if (this.basket[i].sku === sku) {
+        this.basket.splice(i, 1);
+      }
     }
+    return "The item does not exist in your basket!";
+  }
 
-    showBasket() {
-        return this.basket
+  getItemPrice(product) {
+    for (let i = 0; i < inventory.length; i++) {
+      if (inventory[i].sku === product) {
+        return `The price of the item is £${inventory[i].price}`;
+      }
     }
+    return "The product is not available";
+  }
 
-    getBasketTotal() {
-        let totalPrice = 0;
-        for (let i = 0; i < this.basket.length; i++) {
-            totalPrice += this.basket[i].price
-        }
-        return `The total price of the items in your basket is £${Number(totalPrice.toFixed(2))}`
+  showBasket() {
+    return this.basket;
+  }
+
+  getBasketTotal() {
+    let totalPrice = 0;
+    for (let i = 0; i < this.basket.length; i++) {
+      totalPrice += this.basket[i].price;
     }
-};
+    return `The total price of the items in your basket is £${Number(
+      totalPrice.toFixed(2)
+    )}`;
+    // .toFixed() hasn't been created and it's absent on the test.
+  }
+}
+
+// isBasketFull() hasn't been used.
 
 module.exports = Basket;
